31752fcd3d296e9ec461e6eba1b234b76cfd7815
marc vargas <marcvargas@marcs-Air.attlocal.net>
2023-10-06 11:21:08 -0500

    implamented GET /api/users/:id

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -7,22 +7,17 @@ router.get('/', async (req, res) => {
   res.status(200).json(users)
 })
 
-// GET /api/user/:id
+// GET /api/users/:id
 router.get('/:id', async (req, res) => {
   const id = req.params.id
 
-  const results = await User.findAll({
-    where: {
-      id
-    }
-  })
+  const user = await User.findById(id)
 
-  if (results.length === 0) {
-    res.status(404).json({ error: 'Invalid Id Provided' })
-  } else {
-    const user = results[0]
-    res.status(200).json(user)
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' })
   }
+
+  res.status(200).json(user)
 })
 
 module.exports = router
