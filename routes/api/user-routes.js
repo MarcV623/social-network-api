b9206b7946925e5134abce83670ece00b319bc89
marc vargas <marcvargas@marcs-Air.attlocal.net>
2023-10-09 13:58:26 -0500

    DELETE /api/users/:userId/friends/:friendId

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -79,4 +79,31 @@ router.post('/:userId/friends/:friendId', async (req, res) => {
   res.status(200).json({ message: 'User successfully updated' })
 })
 
+// DELETE /api/users/:userId/friends/:friendId
+router.delete('/:userId/friends/:friendId', async (req, res) => {
+  const user = await User.findById(req.params.userId)
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' })
+  }
+
+  const friend = await User.findById(req.params.friendId)
+
+  if (!friend) {
+    return res.status(404).json({ message: 'Friend not found' })
+  }
+
+  if (req.params.userId === req.params.friendId) {
+    return res.status(400).json({ message: 'User & friend ids cannot be the same' })
+  }
+
+  await User.findByIdAndUpdate(req.params.userId, {
+    $pull: {
+      friends: req.params.friendId
+    }
+  })
+
+  res.status(200).json({ message: 'User successfully updated' })
+})
+
 module.exports = router
