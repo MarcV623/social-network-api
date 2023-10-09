c033c6ffb9f75eec195b5b1db36af87ad4c3bacc
marc vargas <marcvargas@marcs-Air.attlocal.net>
2023-10-09 13:51:21 -0500

    implamented POST api/users/:userId/friends/:friendId

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -52,4 +52,31 @@ router.delete('/:id', async (req, res) => {
   res.status(200).json(user)
 })
 
+// POST /api/users/:userId/friends/:friendId
+router.post('/:userId/friends/:friendId', async (req, res) => {
+  const user = await User.findById(req.params.userId)
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' })
+  }
+
+  const friend = await User.findById(req.params.friendId)
+
+  if (!friend) {
+    return res.status(404).json({ message: 'Friend not found' })
+  }
+
+  if (req.params.userId === req.params.friendId) {
+    return res.status(400).json({ message: 'User & friend ids cannot be the same' })
+  }
+
+  await User.findByIdAndUpdate(req.params.userId, {
+    $addToSet: {
+      friends: req.params.friendId
+    }
+  })
+
+  res.status(200).json({ message: 'User successfully updated' })
+})
+
 module.exports = router
